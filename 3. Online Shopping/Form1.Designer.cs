8779d997e1575f8147d9488e5e114de6f939a8b8
Ice713 <icramzilruiz@gmail.com>
2025-11-06 22:35:09 +0800

    Created UI with input and button.

--- 3. Online Shopping/Form1.Designer.cs
+++ 3. Online Shopping/Form1.Designer.cs
@@ -28,13 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDownPrice = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownWarranty = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonDisplayDetails = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPrice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWarranty)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(357, 147);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(120, 20);
+            this.textBoxName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(256, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Product:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(256, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Price:";
+            // 
+            // numericUpDownPrice
+            // 
+            this.numericUpDownPrice.DecimalPlaces = 2;
+            this.numericUpDownPrice.Location = new System.Drawing.Point(357, 173);
+            this.numericUpDownPrice.Maximum = new decimal(new int[] {
+            1215752191,
+            23,
+            0,
+            0});
+            this.numericUpDownPrice.Name = "numericUpDownPrice";
+            this.numericUpDownPrice.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownPrice.TabIndex = 4;
+            // 
+            // numericUpDownWarranty
+            // 
+            this.numericUpDownWarranty.DecimalPlaces = 2;
+            this.numericUpDownWarranty.Location = new System.Drawing.Point(357, 199);
+            this.numericUpDownWarranty.Maximum = new decimal(new int[] {
+            1215752191,
+            23,
+            0,
+            0});
+            this.numericUpDownWarranty.Name = "numericUpDownWarranty";
+            this.numericUpDownWarranty.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownWarranty.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(256, 202);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Warranty (years):";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Elephant", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(238, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(345, 47);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Online Shopping";
+            // 
+            // buttonDisplayDetails
+            // 
+            this.buttonDisplayDetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDisplayDetails.Location = new System.Drawing.Point(300, 246);
+            this.buttonDisplayDetails.Name = "buttonDisplayDetails";
+            this.buttonDisplayDetails.Size = new System.Drawing.Size(134, 65);
+            this.buttonDisplayDetails.TabIndex = 8;
+            this.buttonDisplayDetails.Text = "Display Product Details";
+            this.buttonDisplayDetails.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.buttonDisplayDetails);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDownWarranty);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDownPrice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxName);
+            this.Name = "Form1";
+            this.Text = "3. Online Shopping";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPrice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWarranty)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDownPrice;
+        private System.Windows.Forms.NumericUpDown numericUpDownWarranty;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonDisplayDetails;
     }
 }
 
