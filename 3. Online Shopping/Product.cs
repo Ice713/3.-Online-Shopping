49d02f77e30afabcf52bc609f9e9dcba8628bbb9
Ice713 <icramzilruiz@gmail.com>
2025-11-06 22:37:30 +0800

    Product Class done. Next is Electronic Class.

---
+++ 3. Online Shopping/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Online_Shopping
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public Product(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public virtual string GetDetails()
+        {
+            return $"Product: {Name}\n$Price: {Price}";
+        }
+    }
+}
